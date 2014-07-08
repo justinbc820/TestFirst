e83322c1c46ac8861d8dcde17045e8ce17405934
Justin Christensen <justinbchristensen@gmail.com>
2014-07-07 21:22:24 -0600

    Okay, the real final one, I think

--- 01_properties/properties.js
+++ 01_properties/properties.js
@@ -1,8 +1,11 @@
 function setSomePropertiesOn(object) {
 	object.x =7;
-	object.y = 8;
-	object.onePlus = function (number) {
+	object["y"] = 8;
+	object["onePlus"] = function (number) {
 		return number + 1;
+	}
+	object.onePlus = function (number) {
+		return number +1;
 	}
 
 	return object;

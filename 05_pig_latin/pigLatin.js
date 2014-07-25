d46f270a7709fc9aa7ff7cac04dc761b4a8b6f0c
Justin Christensen <justinchristensen@Justins-MacBook-Pro.local>
2014-07-25 19:53:00 -0400

    Got it so that it will separate all words from a string into separate elements in an array

--- 05_pig_latin/pigLatin.js
+++ 05_pig_latin/pigLatin.js
@@ -1,25 +1,67 @@
-function translate(word) {
-	var firstLetter = word.charAt(0);
-	var secondLetter = word.charAt(1);
-	var translatedWord = ""
+var finalWord = "";
+var wordsArray =[];
+var nextSpacePosition;
 
-	if(firstLetter == ("a"||"e"||"i"||"o"||"u")) {
-		word = word + "ay";
-		return word;
+function translate(word) {
+	finalWord = word;
+	wordsArray = [];
+	nextSpacePosition = 0;
+	needsToBeSplit(word);
+	console.log(wordsArray);
+}
+
+function putIntoArray(word) {
+	wordsArray.push(word);
+}
+
+function needsToBeSplit(word) {
+	nextSpacePosition = word.indexOf(" ");
+	if(nextSpacePosition == -1) {
+		putIntoArray(word);
 	}
 	else {
-		if((firstLetter == "c" || firstLetter == "t") && secondLetter == "h") {
-			firstLetter = firstLetter + secondLetter;
-			word = word.slice(1);
-		}
-		for(var i=0; i<word.length; i++) {
-			translatedWord += word.charAt(i+1);
-		}
-
-		translatedWord += firstLetter + "ay";
-
-		console.log(translatedWord);
-
-		return translatedWord;
+		takeFirstWord(word);
 	}
 }
+
+function takeFirstWord(word) {
+	finalWord = word.slice(nextSpacePosition + 1);
+	word = word.slice(0, nextSpacePosition);
+	putIntoArray(word);
+
+	if(finalWord.indexOf(" ") == -1) {
+		wordsArray.push(finalWord);
+	}
+	else {
+		needsToBeSplit(finalWord);
+	}
+}
+
+
+
+
+
+	//Does it have multiple words?
+		//Yes
+			//Make array with one word per entry
+			//Check Words
+		//No
+			//Check Words
+
+	//Check Words
+		//Does it contain "qu"
+			//Yes
+				//Is it at the beginning?
+					//Yes - Count qu as one letter then translate
+					//No -
+			//No
+		//Does it begin with a vowel or consonant?
+			//Vowel- Translate
+			//Consonant - Does it begin with one, two, or three consonants?
+				//One - Translate
+				//Two - 
+					//Make first two consonants one word
+					//Translate
+				//Three -
+					//Make first three consonants one word
+					//Translate
